654911070e5117da9eb35a39ceb319e8407b3377
Raghav Agarwal <raghav10022001@gmail.com>
2023-12-12 22:25:55 +0530

    First

---
+++ index.js
@@ -0,0 +1,26 @@
+import express from 'express';
+// import connectToMongo from './db.js';
+// import Auth from './Routes/Auth.js';
+// import Transactions from './Routes/Transactions.js';
+import dotenv from 'dotenv';
+dotenv.config();
+const app = express();
+const port = process.env.PORT || 8080;
+// connectToMongo();
+
+app.use((req,res,next)=>{
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', '*');
+    next();
+});
+
+app.use(express.json());
+app.get('/',(req,res)=>{
+    res.send("Hello");
+})
+// app.use('/',Auth);
+// app.use('/transaction/',Transactions);
+
+app.listen(port,()=>{
+    console.log('Server is running on port ' + port);
+})
